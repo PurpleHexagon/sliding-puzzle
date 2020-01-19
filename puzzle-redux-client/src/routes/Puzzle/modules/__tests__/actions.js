5ecd5ee4049c5c3452784d2fffcddf5a2c456c4e
Jon Day <jon@plantigrate.com>
2020-01-19 13:48:54 +0000

    Action and reducer unit tests

--- puzzle-redux-client/src/routes/Puzzle/modules/__tests__/actions.js
+++ puzzle-redux-client/src/routes/Puzzle/modules/__tests__/actions.js
@@ -2,9 +2,9 @@ const chai = require('chai')
 const { expect } = chai
 import sinon from 'sinon'
 import sinonChai from 'sinon-chai'
-import { START_PUZZLE, startPuzzle } from '../puzzle'
+import {moveBlock, MOVE, resetMoveCount, RESET_MOVE_COUNT, startPuzzle, START_PUZZLE, MOVE_COUNT} from '../puzzle'
 import axios from 'axios'
-chai.use(sinonChai);
+chai.use(sinonChai)
 
 describe('actions', () => {
   let sandbox
@@ -33,6 +33,63 @@ describe('actions', () => {
         {
           type: START_PUZZLE,
           payload: []
+        }
+      )
+    })
+  })
+
+  describe('resetMoveCount', () => {
+    let newState
+    let dispatch
+
+    beforeEach(() => {
+      dispatch = sinon.spy()
+
+      newState = resetMoveCount()(dispatch)
+      return newState
+    })
+
+    it('should call dispatch', () => {
+      expect(dispatch).to.have.been.calledWith(
+        {
+          type: RESET_MOVE_COUNT,
+          payload: {}
+        }
+      )
+    })
+  })
+
+  describe('moveBlock', () => {
+    let newState
+    let dispatch
+
+    beforeEach(() => {
+      dispatch = sinon.spy()
+      const resolved = new Promise((r) => r({ data: [] }))
+      sandbox.stub(axios, 'post').returns(resolved)
+
+      const getState = () => ({ puzzle: { token: '1234' } })
+
+      newState = moveBlock()(dispatch, getState)
+      return newState
+    })
+
+    it('should call get', () => {
+      expect(axios.post).to.have.been.calledWith('http://0.0.0.0:8080/move')
+    })
+
+    it('should call dispatch', () => {
+      expect(dispatch).to.have.been.calledWith(
+        {
+          type: MOVE,
+          payload: []
+        }
+      )
+
+      expect(dispatch).to.have.been.calledWith(
+        {
+          type: MOVE_COUNT,
+          payload: {}
         }
       )
     })
--- puzzle-redux-client/src/routes/Puzzle/modules/__tests__/reducers.js
+++ puzzle-redux-client/src/routes/Puzzle/modules/__tests__/reducers.js
@@ -1,6 +1,6 @@
 const chai = require('chai')
 const { expect } = chai
-import puzzleReducer, { MOVE_COUNT, RESET_MOVE_COUNT } from '../puzzle'
+import puzzleReducer, {MOVE, MOVE_COUNT, RESET_MOVE_COUNT, START_PUZZLE} from '../puzzle'
 
 describe('reducers', () => {
   describe('RESET_MOVE_COUNT', () => {
@@ -9,7 +9,6 @@ describe('reducers', () => {
     beforeEach(() => {
       newState = puzzleReducer(
         { moveCount: 2 }, { type: RESET_MOVE_COUNT, payload: {} })
-      console.log(newState)
       return newState
     })
 
@@ -31,6 +30,36 @@ describe('reducers', () => {
     it('should increment the count', () => {
       expect(newState).to.be.eql(
         { moveCount: 3 }
+      )
+    })
+  })
+
+  describe('START_PUZZLE', () => {
+    let newState
+
+    beforeEach(() => {
+      newState = puzzleReducer({ tiles: [] }, { type: START_PUZZLE, payload: { tiles: [1, 2, 3] } })
+      return newState
+    })
+
+    it('should set the tiles', () => {
+      expect(newState).to.be.eql(
+        { tiles: [1, 2, 3] }
+      )
+    })
+  })
+
+  describe('MOVE', () => {
+    let newState
+
+    beforeEach(() => {
+      newState = puzzleReducer({ tiles: [1, 2, 3, 4] }, { type: MOVE, payload: { tiles: [1, 2, 4, 3] } })
+      return newState
+    })
+
+    it('should replace the tiles', () => {
+      expect(newState).to.be.eql(
+        { tiles: [1, 2, 4, 3] }
       )
     })
   })
